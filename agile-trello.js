ebe74ae4ce12cf9716f8f14ac73e6f4c22438f9c
Richard Kotze <rkotze@findmypast.com>
2019-05-28 18:11:42 +0100

    Check a trello key and token is set before making request

--- agile-trello.js
+++ agile-trello.js
@@ -36,34 +36,36 @@ function setupBoardObserver() {
 }
 
 async function cardAge() {
-  const cards = document.querySelectorAll(".list-card");
-  //TODO get the board id from the url
-  const response = await fetch(
-    baseUrl + "/1/boards/QPquSDva/cards?" + authTokenParams
-  );
-  const cardData = await response.json();
-  const cardDataById = [];
-  cardData.map(card => (cardDataById[card.shortLink] = card));
-  cards.forEach(card => {
-    const id = /https:\/\/trello.com\/c\/([A-Za-z0-9]+)\/\S+/.exec(
-      card.href
-    )[1];
-
-    const colInfo = document.createElement("span");
-    colInfo.setAttribute(
-      "style",
-      "text-align:right;font-size: 12px;margin-right:10px;"
+  if (apiKey !== "" && token != "") {
+    const cards = document.querySelectorAll(".list-card");
+    //TODO get the board id from the url
+    const response = await fetch(
+      baseUrl + "/1/boards/QPquSDva/cards?" + authTokenParams
     );
-    colInfo.innerText =
-      "Last changed: " +
-      daysBetween(new Date(cardDataById[id].dateLastActivity), new Date()) +
-      " days ago";
-    card.append(colInfo);
-  });
+    const cardData = await response.json();
+    const cardDataById = [];
+    cardData.map(card => (cardDataById[card.shortLink] = card));
+    cards.forEach(card => {
+      const id = /https:\/\/trello.com\/c\/([A-Za-z0-9]+)\/\S+/.exec(
+        card.href
+      )[1];
 
-  function daysBetween(date1, date2) {
-    var oneDay = 1000 * 60 * 60 * 24;
-    var diff = date2.getTime() - date1.getTime();
-    return Math.round(diff / oneDay);
+      const colInfo = document.createElement("span");
+      colInfo.setAttribute(
+        "style",
+        "text-align:right;font-size: 12px;margin-right:10px;"
+      );
+      colInfo.innerText =
+        "Last changed: " +
+        daysBetween(new Date(cardDataById[id].dateLastActivity), new Date()) +
+        " days ago";
+      card.append(colInfo);
+    });
+
+    function daysBetween(date1, date2) {
+      var oneDay = 1000 * 60 * 60 * 24;
+      var diff = date2.getTime() - date1.getTime();
+      return Math.round(diff / oneDay);
+    }
   }
 }
