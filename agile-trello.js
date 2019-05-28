7d4fe0a474d2f39a0dc56b854b3b86617a6174aa
Richard Kotze <rkotze@findmypast.com>
2019-05-28 17:28:22 +0100

    Using the mutation observer to update totals when dom changes
    This is better an using a timeout because its more real time feel and only runs when changes have happened.

--- agile-trello.js
+++ agile-trello.js
@@ -1,45 +1,45 @@
-import {
-  cardTotalForColumns,
-  updateCardTotals,
-  estimatePointsForCards
-} from './card-totals.js';
-const baseUrl = 'https://api.trello.com';
-const token = ''; //see https://trello.com/app-key
-const apiKey = '';
+import { updateCardTotals, estimatePointsForCards } from "./card-totals.js";
+const baseUrl = "https://api.trello.com";
+const token = ""; //see https://trello.com/app-key
+const apiKey = "";
 const authTokenParams = `key=${apiKey}&token=${token}`;
 
 document.onreadystatechange = function() {
-  if (document.readyState === 'complete') {
+  if (document.readyState === "complete") {
     setTimeout(cardAge, 2000);
-    setTimeout(cardTotalForColumns, 7000); // hacks -> wait for all columns to load
     setTimeout(estimatePointsForCards, 7000);
-    setTimeout(observeDomChanges, 7000);
   }
 };
 
-function observeDomChanges() {
-  let mutationObserver = new MutationObserver(function(mutations) {
+setupBoardObserver()();
+function setupBoardObserver() {
+  const mutationObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
       updateCardTotals(mutation.target);
     });
   });
-
-  const board = document.getElementsByClassName('board-main-content')[0];
-  mutationObserver.observe(board, {
-    attributes: false,
-    characterData: false,
-    childList: true,
-    subtree: true,
-    attributeOldValue: false,
-    characterDataOldValue: false
-  });
+  return function observeDomChanges() {
+    const board = document.getElementsByClassName("board-main-content")[0];
+    if (board) {
+      mutationObserver.observe(board, {
+        attributes: false,
+        characterData: false,
+        childList: true,
+        subtree: true,
+        attributeOldValue: false,
+        characterDataOldValue: false
+      });
+    } else {
+      setTimeout(observeDomChanges, 500);
+    }
+  };
 }
 
 async function cardAge() {
-  const cards = document.querySelectorAll('.list-card');
+  const cards = document.querySelectorAll(".list-card");
   //TODO get the board id from the url
   const response = await fetch(
-    baseUrl + '/1/boards/QPquSDva/cards?' + authTokenParams
+    baseUrl + "/1/boards/QPquSDva/cards?" + authTokenParams
   );
   const cardData = await response.json();
   const cardDataById = [];
@@ -49,15 +49,15 @@ async function cardAge() {
       card.href
     )[1];
 
-    const colInfo = document.createElement('span');
+    const colInfo = document.createElement("span");
     colInfo.setAttribute(
-      'style',
-      'text-align:right;font-size: 12px;margin-right:10px;'
+      "style",
+      "text-align:right;font-size: 12px;margin-right:10px;"
     );
     colInfo.innerText =
-      'Last changed: ' +
+      "Last changed: " +
       daysBetween(new Date(cardDataById[id].dateLastActivity), new Date()) +
-      ' days ago';
+      " days ago";
     card.append(colInfo);
   });
 
