c3efc0da3cb373b30a1ff2a0dda21aefc1f16343
Richard Kotze <rkotze@findmypast.com>
2019-06-05 18:18:34 +0100

    WIP: Save settings in popup for Trello Agile to user profile
    - Colour for ticket id do not change

--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,48 @@
 window.addEventListener("load", function() {
   let popupForm = document.getElementById("agileConfig");
-  function sendData() {
-    let agileConfig = new FormData(popupForm);
-    // console.log("TCL: sendData -> form", popupForm, agileConfig);
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        primary: agileConfig.get("primary")
-      });
-    });
-  }
+  getSavedValues(values => {
+    sendDataToTab(values);
+    setFormValues(popupForm, values);
+  });
 
   popupForm.addEventListener("submit", function(event) {
     event.preventDefault();
-
-    sendData();
+    const values = getFormValues(popupForm);
+    saveValues(values);
+    sendDataToTab(values);
   });
 });
+
+function sendDataToTab(values) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, values);
+  });
+}
+
+function getFormValues(formElement) {
+  if (formElement) {
+    let agileConfig = new FormData(formElement);
+    return {
+      idForeground: agileConfig.get("idForeground"),
+      pointsBackground: agileConfig.get("pointsBackground")
+    };
+  }
+  return null;
+}
+
+function setFormValues(formElement, values) {
+  for (let i = 0; i < formElement.length; i++) {
+    const input = formElement[i];
+    if (Object.keys(values).includes(input.id)) {
+      input.value = values[input.id];
+    }
+  }
+}
+
+function getSavedValues(fn) {
+  chrome.storage.sync.get(["idForeground", "pointsBackground"], fn);
+}
+
+function saveValues({ idForeground, pointsBackground }) {
+  chrome.storage.sync.set({ idForeground, pointsBackground });
+}
